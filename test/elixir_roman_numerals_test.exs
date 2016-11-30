a76f4abfbf42f53970ef44fab8b626a2765f842b
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-11-30 13:58:18 +0000

    Refactor tests to hide implementation details from test description

--- test/elixir_roman_numerals_test.exs
+++ test/elixir_roman_numerals_test.exs
@@ -1,96 +1,96 @@
 defmodule NumberConverterTest do
   use ExUnit.Case, async: true
 
-  test "&arabic_to_numeral/1 returns an empty string when given 0" do
-    assert NumberConverter.arabic_to_numeral(0) == ""
-  end
+  describe "arabic to numeral conversion" do
+    test "converts 0 appropriately" do
+      assert NumberConverter.arabic_to_numeral(0) == ""
+    end
 
-  test "&arabic_to_numeral/1 returns I when given 1" do
-    assert NumberConverter.arabic_to_numeral(1) == "I"
-  end
+    test "converts 1 appropriately" do
+      assert NumberConverter.arabic_to_numeral(1) == "I"
+    end
 
-  test "&arabic_to_numeral/1 returns II when given 2" do
-    assert NumberConverter.arabic_to_numeral(2) == "II"
-  end
+    test "converts 2 appropriately" do
+      assert NumberConverter.arabic_to_numeral(2) == "II"
+    end
 
-  test "&arabic_to_numeral/1 returns IV when given 4" do
-    assert NumberConverter.arabic_to_numeral(4) == "IV"
-  end
+    test "converts 4 appropriately" do
+      assert NumberConverter.arabic_to_numeral(4) == "IV"
+    end
 
-  test "&arabic_to_numeral/1 returns V when given 5" do
-    assert NumberConverter.arabic_to_numeral(5) == "V"
-  end
+    test "converts 5 appropriately" do
+      assert NumberConverter.arabic_to_numeral(5) == "V"
+    end
 
-  test "&arabic_to_numeral/1 returns VI when given 6" do
-    assert NumberConverter.arabic_to_numeral(6) == "VI"
-  end
+    test "converts 6 appropriately" do
+      assert NumberConverter.arabic_to_numeral(6) == "VI"
+    end
 
-  test "&arabic_to_numeral/1 returns X when given 10" do
-    assert NumberConverter.arabic_to_numeral(10) == "X"
-  end
+    test "converts 10 appropriately" do
+      assert NumberConverter.arabic_to_numeral(10) == "X"
+    end
 
-  test "&arabic_to_numeral/1 returns L when given 50" do
-    assert NumberConverter.arabic_to_numeral(50) == "L"
-  end
+    test "converts 50 appropriately" do
+      assert NumberConverter.arabic_to_numeral(50) == "L"
+    end
 
-  test "&arabic_to_numeral/1 returns C when given 100" do
-    assert NumberConverter.arabic_to_numeral(100) == "C"
-  end
+    test "converts 100 appropriately" do
+      assert NumberConverter.arabic_to_numeral(100) == "C"
+    end
 
-  test "&arabic_to_numeral/1 returns D when given 500" do
-    assert NumberConverter.arabic_to_numeral(500) == "D"
-  end
+    test "converts 500 appropriately" do
+      assert NumberConverter.arabic_to_numeral(500) == "D"
+    end
 
-  test "&arabic_to_numeral/1 returns M when given 1000" do
-    assert NumberConverter.arabic_to_numeral(1000) == "M"
-  end
+    test "converts 1000 appropriately" do
+      assert NumberConverter.arabic_to_numeral(1000) == "M"
+    end
 
-  test "&arabic_to_numeral/1 returns IX when given 9" do
-    assert NumberConverter.arabic_to_numeral(9) == "IX"
-  end
+    test "converts 9 appropriately" do
+      assert NumberConverter.arabic_to_numeral(9) == "IX"
+    end
 
-  test "&arabic_to_numeral/1 returns VII when given 7" do
-    assert NumberConverter.arabic_to_numeral(7) == "VII"
-  end
+    test "converts 7 appropriately" do
+      assert NumberConverter.arabic_to_numeral(7) == "VII"
+    end
 
-  test "&arabic_to_numeral/1 returns XI when given 11" do
-    assert NumberConverter.arabic_to_numeral(11) == "XI"
-  end
+    test "converts 11 appropriately" do
+      assert NumberConverter.arabic_to_numeral(11) == "XI"
+    end
 
-  test "&arabic_to_numeral/1 returns XX when given 20" do
-    assert NumberConverter.arabic_to_numeral(20) == "XX"
-  end
+    test "converts 20 appropriately" do
+      assert NumberConverter.arabic_to_numeral(20) == "XX"
+    end
+test "converts 21 appropriately" do
+      assert NumberConverter.arabic_to_numeral(21) == "XXI"
+    end
 
-  test "&arabic_to_numeral/1 returns XX when given 21" do
-    assert NumberConverter.arabic_to_numeral(21) == "XXI"
-  end
+    test "converts 19 appropriately" do
+      assert NumberConverter.arabic_to_numeral(19) == "XIX"
+    end
 
-  test "&arabic_to_numeral/1 returns XIX when given 19" do
-    assert NumberConverter.arabic_to_numeral(19) == "XIX"
-  end
+    test "converts 40 appropriately" do
+      assert NumberConverter.arabic_to_numeral(40) == "XL"
+    end
 
-  test "&arabic_to_numeral/1 returns XL when given 40" do
-    assert NumberConverter.arabic_to_numeral(40) == "XL"
-  end
+    test "converts 90 appropriately" do
+      assert NumberConverter.arabic_to_numeral(90) == "XC"
+    end
 
-  test "&arabic_to_numeral/1 returns XL when given 90" do
-    assert NumberConverter.arabic_to_numeral(90) == "XC"
-  end
+    test "converts 400 appropriately" do
+      assert NumberConverter.arabic_to_numeral(400) == "CD"
+    end
 
-  test "&arabic_to_numeral/1 returns CD when given 400" do
-    assert NumberConverter.arabic_to_numeral(400) == "CD"
-  end
+    test "converts 900 appropriately" do
+      assert NumberConverter.arabic_to_numeral(900) == "CM"
+    end
 
-  test "&arabic_to_numeral/1 returns CM when given 900" do
-    assert NumberConverter.arabic_to_numeral(900) == "CM"
-  end
+    test "converts 2947 appropriately" do
+      assert NumberConverter.arabic_to_numeral(2947) == "MMCMXLVII"
+    end
 
-  test "&arabic_to_numeral/1 returns MMCMXLVII when given 2947" do
-    assert NumberConverter.arabic_to_numeral(2947) == "MMCMXLVII"
+    test "converts 1534 appropriately" do
+      assert NumberConverter.arabic_to_numeral(1534) == "MDXXXIV"
+    end
   end
-
-  test "&arabic_to_numeral/1 returns MDXXXIV when given 1534" do
-    assert NumberConverter.arabic_to_numeral(1534) == "MDXXXIV"
-  end
-  
 end
